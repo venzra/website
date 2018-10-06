ca2a412df02e4aa49b723c4512ac576c30130d01
Andrew Cunliffe <andrew@sysen.co.uk>
2018-10-06 12:28:07 +0100

    Added PWA capabilities and general improvements based on lighthouse audit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { VenzraModule } from '../components/venzra.module';
 import { VenzraRoutingModule } from '../components/routing.module';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -13,6 +15,7 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
         BrowserAnimationsModule,
         VenzraModule,
         VenzraRoutingModule
